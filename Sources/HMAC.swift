8148a59dd8083d052b40377b703d6745768544f8
Bill Abt <babt@us.ibm.com>
2016-04-21 14:55:03 -0400

    HMAC now supports both OS X (CommonCrypto) and Linux (OpenSSL). Overall package is still OS X only.

--- Sources/HMAC.swift
+++ Sources/HMAC.swift
@@ -16,13 +16,20 @@
 //
 
 import Foundation
-import CommonCrypto
+
+#if os(OSX)
+	import CommonCrypto
+#elseif os(Linux)
+	import CCrypto
+#endif
 
 ///
 /// Calculates a cryptographic Hash-Based Message Authentication Code (HMAC).
 ///
-public class HMAC : Updateable
-{
+public class HMAC : Updateable {
+	
+	static let usingSSL = true
+
     ///
     /// Enumerates available algorithms.
     ///
@@ -30,14 +37,19 @@ public class HMAC : Updateable
 		
         /// Message Digest 5
         case MD5,
+		
         /// Secure Hash Algorithm 1
             SHA1,
+		
         /// Secure Hash Algorithm 2 224-bit
             SHA224,
+		
         /// Secure Hash Algorithm 2 256-bit
             SHA256,
+		
         /// Secure Hash Algorithm 2 384-bit
             SHA384,
+		
         /// Secure Hash Algorithm 2 512-bit
             SHA512
         
@@ -48,27 +60,50 @@ public class HMAC : Updateable
             CCHmacAlgorithm(kCCHmacAlgSHA384):.SHA384,
             CCHmacAlgorithm(kCCHmacAlgSHA512):.SHA512,
             CCHmacAlgorithm(kCCHmacAlgSHA224):.SHA224 ]
-        
-        func nativeValue() -> CCHmacAlgorithm {
+		
+		#if os(OSX)
+		
+			func nativeValue() -> CCHmacAlgorithm {
 			
-            switch self {
+				switch self {
 				
-            case .SHA1:
-                return CCHmacAlgorithm(kCCHmacAlgSHA1)
-            case .MD5:
-                return CCHmacAlgorithm(kCCHmacAlgMD5)
-            case .SHA224:
-                return CCHmacAlgorithm(kCCHmacAlgSHA224)
-            case .SHA256:
-                return CCHmacAlgorithm(kCCHmacAlgSHA256)
-            case .SHA384:
-                return CCHmacAlgorithm(kCCHmacAlgSHA384)
-            case .SHA512:
-                return CCHmacAlgorithm(kCCHmacAlgSHA512)
-                
-            }
-        }
-        
+				case .SHA1:
+					return CCHmacAlgorithm(kCCHmacAlgSHA1)
+				case .MD5:
+					return CCHmacAlgorithm(kCCHmacAlgMD5)
+				case .SHA224:
+					return CCHmacAlgorithm(kCCHmacAlgSHA224)
+				case .SHA256:
+					return CCHmacAlgorithm(kCCHmacAlgSHA256)
+				case .SHA384:
+					return CCHmacAlgorithm(kCCHmacAlgSHA384)
+				case .SHA512:
+					return CCHmacAlgorithm(kCCHmacAlgSHA512)
+				}
+			}
+		
+		#elseif os(Linux)
+		
+			func nativeValue() -> UnsafePointer<EVP_MD> {
+		
+				switch self {
+		
+				case .SHA1:
+					fatalError("SHA1 is not supported by OpenSSL")
+				case .MD5:
+					return EVP_md5()
+				case .SHA224:
+					return EVP_sha224()
+				case .SHA256:
+					return EVP_sha256()
+				case .SHA384:
+					return EVP_sha384()
+				case .SHA512:
+					return EVP_sha512()
+				}
+			}
+		#endif
+		
 		static func fromNativeValue(nativeAlg : CCHmacAlgorithm) -> Algorithm? {
 			
             return fromNative[nativeAlg]
@@ -79,25 +114,50 @@ public class HMAC : Updateable
         ///
         public func digestLength() -> Int {
 			
-            switch self {
+			#if os(OSX)
 				
-            case .SHA1:
-                return Int(CC_SHA1_DIGEST_LENGTH)
-            case .MD5:
-                return Int(CC_MD5_DIGEST_LENGTH)
-            case .SHA224:
-                return Int(CC_SHA224_DIGEST_LENGTH)
-            case .SHA256:
-                return Int(CC_SHA256_DIGEST_LENGTH)
-            case .SHA384:
-                return Int(CC_SHA384_DIGEST_LENGTH)
-            case .SHA512:
-                return Int(CC_SHA512_DIGEST_LENGTH)
-            }
+				switch self {
+					
+				case .SHA1:
+					return Int(CC_SHA1_DIGEST_LENGTH)
+				case .MD5:
+					return Int(CC_MD5_DIGEST_LENGTH)
+				case .SHA224:
+					return Int(CC_SHA224_DIGEST_LENGTH)
+				case .SHA256:
+					return Int(CC_SHA256_DIGEST_LENGTH)
+				case .SHA384:
+					return Int(CC_SHA384_DIGEST_LENGTH)
+				case .SHA512:
+					return Int(CC_SHA512_DIGEST_LENGTH)
+				}
+				
+			#elseif os(Linux)
+				switch self {
+					
+				case .SHA1:
+					fatalError("SHA1 is not supported by OpenSSL")
+				case .MD5:
+					return Int(MD5_DIGEST_LENGTH)
+				case .SHA224:
+					return Int(SHA224_DIGEST_LENGTH)
+				case .SHA256:
+					return Int(SHA256_DIGEST_LENGTH)
+				case .SHA384:
+					return Int(SHA384_DIGEST_LENGTH)
+				case .SHA512:
+					return Int(SHA512_DIGEST_LENGTH)
+				}
+			#endif
+			
         }
     }
-    
-    typealias Context = UnsafeMutablePointer<CCHmacContext>
+	
+	#if os(OSX)
+    	typealias Context = UnsafeMutablePointer<CCHmacContext>
+	#elseif os(Linux)
+		typealias Context = UnsafeMutablePointer<HMAC_CTX>
+	#endif
     
     /// Status of the calculation
     public var status: Status = .Success
@@ -110,51 +170,71 @@ public class HMAC : Updateable
 	///
 	/// Creates a new HMAC instance with the specified algorithm and key.
 	///
-	/// - parameter algorithm: selects the algorithm
-	/// - parameter keyBuffer: specifies the key
-	///	- parameter keyByteCount: number of bytes on keyBuffer
+	/// - Parameters:
+ 	///		- algorithm: 	selects the algorithm
+	/// 	- keyBuffer: 	specifies the key
+	///		- keyByteCount: number of bytes on keyBuffer
 	///
 	init(algorithm: Algorithm, keyBuffer: UnsafePointer<Void>, keyByteCount: Int) {
 		
         self.algorithm = algorithm
-        CCHmacInit(context, algorithm.nativeValue(), keyBuffer, size_t(keyByteCount))
+		#if os(OSX)
+	        CCHmacInit(context, algorithm.nativeValue(), keyBuffer, size_t(keyByteCount))
+		#elseif os(Linux)
+			HMAC_Init(context, keyBuffer, Int32(keyByteCount), algorithm.nativeValue())
+		#endif
     }
     
     ///
     /// Creates a new HMAC instance with the specified algorithm and key.
     ///
-    /// - parameter algorithm: selects the algorithm
-    /// - parameter key: specifies the key
+    /// - Parameters:
+ 	///		- algorithm: 	selects the algorithm
+    /// 	- key: 			specifies the key
     ///
-	public init(algorithm : Algorithm, key : NSData) {
+	public init(algorithm: Algorithm, key: NSData) {
 		
         self.algorithm = algorithm
-        CCHmacInit(context, algorithm.nativeValue(), key.bytes, size_t(key.length))
+		#if os(OSX)
+        	CCHmacInit(context, algorithm.nativeValue(), key.bytes, size_t(key.length))
+		#elseif os(Linux)
+			HMAC_Init(context, key.bytes, Int32(key.length), algorithm.nativeValue())
+		#endif
     }
     
     ///
     /// Creates a new HMAC instance with the specified algorithm and key.
     ///
-    /// - parameter algorithm: selects the algorithm
-    /// - parameter key: specifies the key
+    /// - Parameters:
+ 	///		- algorithm: 	selects the algorithm
+    /// 	- key: 			specifies the key
     ///
-	public init(algorithm : Algorithm, key : [UInt8]) {
+	public init(algorithm: Algorithm, key: [UInt8]) {
 		
         self.algorithm = algorithm
-        CCHmacInit(context, algorithm.nativeValue(), key, size_t(key.count))
+		#if os(OSX)
+        	CCHmacInit(context, algorithm.nativeValue(), key, size_t(key.count))
+		#elseif os(Linux)
+			HMAC_Init(context, key, Int32(key.count), algorithm.nativeValue())
+		#endif
     }
     
     ///
     /// Creates a new HMAC instance with the specified algorithm and key string.
     /// The key string is converted to bytes using UTF8 encoding.
     ///
-    /// - parameter algorithm: selects the algorithm
-    /// - parameter key: specifies the key
+    /// - Parameters:
+ 	///		- algorithm: 	selects the algorithm
+    /// 	- key: 			specifies the key
     ///
-	public init(algorithm : Algorithm, key : String) {
+	public init(algorithm: Algorithm, key: String) {
 		
         self.algorithm = algorithm
-        CCHmacInit(context, algorithm.nativeValue(), key, size_t(key.lengthOfBytes(using:)(using: NSUTF8StringEncoding)))
+		#if os(OSX)
+        	CCHmacInit(context, algorithm.nativeValue(), key, size_t(key.lengthOfBytes(using: NSUTF8StringEncoding)))
+		#elseif os(Linux)
+			HMAC_Init(context, key, Int32(key.lengthOfBytes(using: NSUTF8StringEncoding)), algorithm.nativeValue())
+		#endif
     }
     
     deinit {
@@ -166,25 +246,34 @@ public class HMAC : Updateable
     ///
     /// Updates the calculation of the HMAC with the contents of a buffer.
 	///
-	/// - parameter buffer: update buffer
+	/// - Parameter buffer: update buffer
     ///
-    /// - returns: the calculated HMAC
+    /// - Returns: the calculated HMAC
     ///
-    public func update(buffer: UnsafePointer<Void>, byteCount : size_t) -> Self?
-    {
-        CCHmacUpdate(context, buffer, byteCount)
-        return self 
+	public func update(buffer: UnsafePointer<Void>, byteCount: size_t) -> Self? {
+		
+		#if os(OSX)
+	        CCHmacUpdate(context, buffer, byteCount)
+		#elseif os(Linux)
+			HMAC_Update(context, UnsafePointer<UInt8>(buffer), byteCount)
+		#endif
+        return self
     }
     
     ///
     /// Finalizes the HMAC calculation
     ///
-    /// - returns: the calculated HMAC
+    /// - Returns: the calculated HMAC
     ///
-    public func final() -> [UInt8]
-    {
+	public func final() -> [UInt8] {
+		
 		var hmac = Array<UInt8>(repeating: 0, count:algorithm.digestLength())
-        CCHmacFinal(context, &hmac)
+		#if os(OSX)
+        	CCHmacFinal(context, &hmac)
+		#elseif os(Linux)
+			var length: UInt32 = 0
+			HMAC_Final(context, &hmac, &length)
+		#endif
         return hmac
     }
 }
