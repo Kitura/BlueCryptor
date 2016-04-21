1ece5552fae5c7d0857d42c8655d3ede62311fd8
Bill Abt <babt@us.ibm.com>
2016-04-21 16:33:11 -0400

    PKCS5_PBKDF2_HMAC now supports both OS X (CommonCrypto) and Linux (OpenSSL). Overall package is still OS X only.

--- Sources/KeyDerivation.swift
+++ Sources/KeyDerivation.swift
@@ -16,126 +16,188 @@
 //
 
 import Foundation
-import CommonCrypto
+
+#if os(OSX)
+	import CommonCrypto
+#elseif os(Linux)
+	import CCrypto
+#endif
 
 ///
 /// Derives key material from a password or passphrase.
 ///
 public class PBKDF {
+	
+	static let usingSSL = true
 
     /// Enumerates available pseudo random algorithms
 	public enum PseudoRandomAlgorithm {
 		
         /// Secure Hash Algorithm 1
         case SHA1
+		
         /// Secure Hash Algorithm 2 224-bit
         case SHA224
+		
         /// Secure Hash Algorithm 2 256-bit
         case SHA256
+		
         /// Secure Hash Algorithm 2 384-bit
         case SHA384
+		
         /// Secure Hash Algorithm 2 512-bit
         case SHA512
-        
-		func nativeValue() -> CCPseudoRandomAlgorithm {
+		
+		#if os(OSX)
+			func nativeValue() -> CCPseudoRandomAlgorithm {
 			
-            switch self {
-                case SHA1:
+            	switch self {
+		
+	            case SHA1:
 					return CCPseudoRandomAlgorithm(kCCPRFHmacAlgSHA1)
-                case SHA224:
+				case SHA224:
 					return CCPseudoRandomAlgorithm(kCCPRFHmacAlgSHA224)
-                case SHA256:
+				case SHA256:
 					return CCPseudoRandomAlgorithm(kCCPRFHmacAlgSHA256)
-                case SHA384:
+				case SHA384:
 					return CCPseudoRandomAlgorithm(kCCPRFHmacAlgSHA384)
-                case SHA512:
+				case SHA512:
 					return CCPseudoRandomAlgorithm(kCCPRFHmacAlgSHA512)
-                
-            }
-        }
+        	    }
+        	}
+		
+		#elseif os(Linux)
+		
+			func nativeValue() -> UnsafePointer<EVP_MD> {
+			
+				switch self {
+					
+				case SHA1:
+					return EVP_sha1()
+				case .SHA224:
+					return EVP_sha224()
+				case .SHA256:
+					return EVP_sha256()
+				case .SHA384:
+					return EVP_sha384()
+				case .SHA512:
+					return EVP_sha512()
+				}
+			}
+		#endif
     }
-    
+	
     ///
     /// Determines the (approximate) number of iterations of the key derivation algorithm that need
     /// to be run to achieve a particular delay (or calculation time).
     ///
-    /// - parameter passwordLength: password length in bytes
-    /// - parameter saltLength: salt length in bytes
-    /// - parameter algorithm: the PseudoRandomAlgorithm to use
-    /// - parameter derivedKeyLength: the desired key length
-    /// - parameter msec: the desired calculation time
-    /// - returns: the number of times the algorithm should be run
+    /// - Parameters:
+ 	///		- passwordLength: password length in bytes
+    /// 	- saltLength: salt length in bytes
+    /// 	- algorithm: the PseudoRandomAlgorithm to use
+    /// 	- derivedKeyLength: the desired key length
+    /// 	- msec: the desired calculation time
+	///
+    /// - Returns: the number of times the algorithm should be run
     ///
 	public class func calibrate(passwordLength: Int, saltLength: Int, algorithm: PseudoRandomAlgorithm, derivedKeyLength: Int, msec : UInt32) -> UInt {
-		
-        return UInt(CCCalibratePBKDF(CCPBKDFAlgorithm(kCCPBKDF2), passwordLength, saltLength, algorithm.nativeValue(), derivedKeyLength, msec))
+		#if os(OSX)
+	        return UInt(CCCalibratePBKDF(CCPBKDFAlgorithm(kCCPBKDF2), passwordLength, saltLength, algorithm.nativeValue(), derivedKeyLength, msec))
+		#elseif os(Linux)
+			// Value us per RFC 2898.
+			return UInt(1000)
+		#endif
     }
     
 
     /// 
     /// Derives key material from a password and salt.
     ///
-    /// -parameter password: the password string, will be converted using UTF8
-    /// -parameter salt: the salt string will be converted using UTF8
-    /// -parameter prf: the pseudo random function
-    /// -parameter round: the number of rounds
-    /// -parameter derivedKeyLength: the length of the desired derived key, in bytes.
-    /// -returns: the derived key
+    /// - Parameters:
+ 	///		- password: the password string, will be converted using UTF8
+    /// 	- salt: the salt string will be converted using UTF8
+    /// 	- prf: the pseudo random function
+    /// 	- round: the number of rounds
+    /// 	- derivedKeyLength: the length of the desired derived key, in bytes.
+	///
+    /// - Returns: the derived key
     ///
 	public class func deriveKey(password: String, salt: String, prf:PseudoRandomAlgorithm, rounds: uint, derivedKeyLength: UInt) -> [UInt8] {
 		
 		var derivedKey = Array<UInt8>(repeating: 0, count:Int(derivedKeyLength))
-		let status : Int32 = CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2), password, password.lengthOfBytes(using: NSUTF8StringEncoding), salt, salt.lengthOfBytes(using:NSUTF8StringEncoding), prf.nativeValue(), rounds, &derivedKey, derivedKey.count)
-		if status != Int32(kCCSuccess) {
+		#if os(OSX)
+			let status: Int32 = CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2), password, password.lengthOfBytes(using: NSUTF8StringEncoding), salt, salt.lengthOfBytes(using:NSUTF8StringEncoding), prf.nativeValue(), rounds, &derivedKey, derivedKey.count)
+			if status != Int32(kCCSuccess) {
 			
-            NSLog("ERROR: CCKeyDerivationPBDK failed with stats \(status).")
-            fatalError("ERROR: CCKeyDerivationPBDK failed.")
-        }
+    	        fatalError("ERROR: CCKeyDerivationPBDK failed with status \(status).")
+        	}
+		#elseif os(Linux)
+			let status = PKCS5_PBKDF2_HMAC(password, Int32(password.lengthOfBytes(using: NSUTF8StringEncoding)), salt, Int32(salt.lengthOfBytes(using: NSUTF8StringEncoding)), Int32(rounds), prf.nativeValue(), Int32(derivedKey.count), &derivedKey)
+			if status != 1 {
+				fatalError("ERROR: PKCS5_PBKDF2_HMAC failed.")
+			}
+		#endif
         return derivedKey
     }
     
     ///
     /// Derives key material from a password and salt.
     ///
-    /// -parameter password: the password string, will be converted using UTF8
-    /// -parameter salt: the salt array of bytes
-    /// -parameter prf: the pseudo random function
-    /// -parameter round: the number of rounds
-    /// -parameter derivedKeyLength: the length of the desired derived key, in bytes.
-    /// -returns: the derived key
+    /// - Parameters: 
+	///		- password: the password string, will be converted using UTF8
+    /// 	- salt: the salt array of bytes
+    /// 	- prf: the pseudo random function
+    /// 	- round: the number of rounds
+    /// 	- derivedKeyLength: the length of the desired derived key, in bytes.
+	///
+    /// - Returns: the derived key
     ///
 	public class func deriveKey(password : String, salt : [UInt8], prf: PseudoRandomAlgorithm, rounds: uint, derivedKeyLength: UInt) -> [UInt8] {
 		
 		var derivedKey = Array<UInt8>(repeating: 0, count:Int(derivedKeyLength))
-		let status : Int32 = CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2), password, password.lengthOfBytes(using: NSUTF8StringEncoding), salt, salt.count, prf.nativeValue(), rounds, &derivedKey, derivedKey.count)
-		if status != Int32(kCCSuccess) {
+		#if os(OSX)
+			let status: Int32 = CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2), password, password.lengthOfBytes(using: NSUTF8StringEncoding), salt, salt.count, prf.nativeValue(), rounds, &derivedKey, derivedKey.count)
+			if status != Int32(kCCSuccess) {
 	
-            NSLog("ERROR: CCKeyDerivationPBDK failed with stats \(status).")
-            fatalError("ERROR: CCKeyDerivationPBDK failed.")
-        }
+	            fatalError("ERROR: CCKeyDerivationPBDK failed with status \(status).")
+        	}
+		#elseif os(Linux)
+			let status = PKCS5_PBKDF2_HMAC(password, Int32(password.lengthOfBytes(using: NSUTF8StringEncoding)), salt, Int32(salt.count), Int32(rounds), prf.nativeValue(), Int32(derivedKey.count), &derivedKey)
+			if status != 1 {
+				fatalError("ERROR: PKCS5_PBKDF2_HMAC failed.")
+			}
+		#endif
         return derivedKey
     }
     
     ///
     /// Derives key material from a password buffer.
     ///
-    /// - parameter password: pointer to the password buffer
-    /// - parameter passwordLength: password length in bytes
-    /// - parameter salt: pointer to the salt buffer
-    /// - parameter saltLength: salt length in bytes
-    /// - parameter prf: the PseudoRandomAlgorithm to use
-    /// - parameter rounds: the number of rounds of the algorithm to use
-    /// - parameter derivedKey: pointer to the derived key buffer.
-    /// - parameter derivedKeyLength: the desired key length
-    /// - return: the number of times the algorithm should be run
+    /// - Parameters:
+ 	///		- password: pointer to the password buffer
+    /// 	- passwordLength: password length in bytes
+    /// 	- salt: pointer to the salt buffer
+    /// 	- saltLength: salt length in bytes
+    /// 	- prf: the PseudoRandomAlgorithm to use
+    /// 	- rounds: the number of rounds of the algorithm to use
+    /// 	- derivedKey: pointer to the derived key buffer.
+    /// 	- derivedKeyLength: the desired key length
+	///
+    /// - Returns: the number of times the algorithm should be run
     ///
 	public class func deriveKey(password: UnsafePointer<Int8>, passwordLen: Int, salt: UnsafePointer<UInt8>, saltLen: Int, prf: PseudoRandomAlgorithm, rounds: uint, derivedKey: UnsafeMutablePointer<UInt8>, derivedKeyLen: Int) {
 		
-        let status : Int32 = CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2), password, passwordLen, salt, saltLen, prf.nativeValue(), rounds, derivedKey, derivedKeyLen)
-		if status != Int32(kCCSuccess) {
+		#if os(OSX)
+        	let status: Int32 = CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2), password, passwordLen, salt, saltLen, prf.nativeValue(), rounds, derivedKey, derivedKeyLen)
+			if status != Int32(kCCSuccess) {
 			
-            NSLog("ERROR: CCKeyDerivationPBDK failed with stats \(status).")
-            fatalError("ERROR: CCKeyDerivationPBDK failed.")
-        }
-    }
+	            fatalError("ERROR: CCKeyDerivationPBDK failed with status \(status).")
+    	    }
+		#elseif os(Linux)
+			let status = PKCS5_PBKDF2_HMAC(password, Int32(passwordLen), salt, Int32(saltLen), Int32(rounds), prf.nativeValue(), Int32(derivedKeyLen), derivedKey)
+			if status != 1 {
+				fatalError("ERROR: PKCS5_PBKDF2_HMAC failed.")
+			}
+		#endif
+	}
 }
