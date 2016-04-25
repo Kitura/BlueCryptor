05330a5928488c6e59a0a1408eba574b8b971b47
Bill Abt <babt@us.ibm.com>
2016-04-25 10:58:20 -0400

    Minor edits for consistency.

--- Sources/Updateable.swift
+++ Sources/Updateable.swift
@@ -47,7 +47,7 @@ extension Updateable {
 	public func update(data: NSData) -> Self? {
 		
         update(buffer: data.bytes, byteCount: size_t(data.length))
-        return self.status == Status.Success ? self : nil
+        return self.status == .Success ? self : nil
     }
 	
     ///
@@ -58,7 +58,7 @@ extension Updateable {
 	public func update(byteArray: [UInt8]) -> Self? {
 		
         update(buffer: byteArray, byteCount: size_t(byteArray.count))
-        return self.status == Status.Success ? self : nil
+		return self.status == .Success ? self : nil
     }
 	
     ///
@@ -70,6 +70,6 @@ extension Updateable {
 	public func update(string: String) -> Self? {
 		
         update(buffer: string, byteCount: size_t(string.lengthOfBytes(using: NSUTF8StringEncoding)))
-        return self.status == Status.Success ? self : nil
+		return self.status == .Success ? self : nil
     }
 }
