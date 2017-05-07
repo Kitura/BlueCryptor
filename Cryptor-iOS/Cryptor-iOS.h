c1dcad2aefcb6e90e4aa4d72110abb40a8344871
Bill Abt <babt@us.ibm.com>
2017-05-07 11:47:34 -0400

    Added iOS target to build iOS framework.

---
+++ Cryptor-iOS/Cryptor-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Cryptor-iOS.h
+//  Cryptor-iOS
+//
+//  Created by Bill Abt on 5/7/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cryptor-iOS.
+FOUNDATION_EXPORT double Cryptor_iOSVersionNumber;
+
+//! Project version string for Cryptor-iOS.
+FOUNDATION_EXPORT const unsigned char Cryptor_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cryptor_iOS/PublicHeader.h>
+
+
